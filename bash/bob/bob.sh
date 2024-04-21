7cc603c4cb282c23eb7c132b2e4f21447360db37
Matt McCullough <mattrmcc@gmail.com>
2024-04-20 23:44:05 -0500

    add bob exercise

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,24 +1,48 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+# set -f
+# x=${1//[[:space:]]/}
+# [ -z "$x" ] && echo "Fine. Be that way!" && exit 0
+# if [ "${x: -1}" = "?" ]; then
+#     if [[ "$x" = "${x^^}" ]] && [[ "$x" =~ [A-Z] ]]; then
+#         echo "Calm down, I know what I'm doing!"
+#         exit 0
+#     fi
+#     echo "Sure."
+#     exit 0
+# fi
+# if [[ "$x" = "${x^^}" ]] && [[ "$x" =~ [A-Z] ]]; then
+#     echo "Whoa, chill out!"
+#     exit 0
+# fi
+# echo "Whatever."
+
+
+main () {
+    local -r message=$(echo "$1" | tr -d "[:space:]")
+    if silence; then
+        echo "Fine. Be that way!"
+    elif yelling && question; then
+        echo "Calm down, I know what I'm doing!"
+    elif yelling; then
+        echo "Whoa, chill out!"
+    elif question; then
+        echo "Sure."
+    else
+        echo "Whatever."
+    fi
+}
+
+silence () {
+    [[ -z $message ]]
+}
+
+yelling () {
+    [[ $message == *[[:upper:]]* && $message != *[[:lower:]]* ]]
+}
+
+question () {
+    [[ $message =~ \?$ ]]
+}
+
+main "$@"
