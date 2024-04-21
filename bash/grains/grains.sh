b83e72e189e22259e8bffea418df9cc8d5e11a6d
Matt McCullough <mattrmcc@gmail.com>
2024-04-21 11:25:56 -0500

    update grains exercise

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
 
+total () {
+    value=0
+    for i in {0..63}
+    do
+        value=$(bc <<< "$value+(2^($i))")
+    done
+    echo "$value"
+}
+
+if [ "$1" == "total" ]; then
+    total
+elif [ "$1" -ge 1 ] && [ "$1" -le 64 ]; then
+    bc <<< "2^($1-1)"
+else
+    echo "Error: invalid input"
+    exit 1
+fi
