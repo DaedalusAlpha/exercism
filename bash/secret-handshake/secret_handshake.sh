9a0043cf262a83d60da47134c16aec80d44d70ec
Matt McCullough <mattrmcc@gmail.com>
2024-04-23 13:13:37 -0500

    update secret_handshake script

--- bash/secret-handshake/secret_handshake.sh
+++ bash/secret-handshake/secret_handshake.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/env bash
 
-actions=("wink" "double blink" "close your eyes" "jump")
-# echo ${actions[*]}
+IFS=,
+declare -a actions=("wink" "double blink" "close your eyes" "jump")
 
 number=$(bc<<< "obase=2; $1")
-echo $number
 
-for ((i=1; i<=${#number}; i++)); do
-    index=${number:$((-i)):1}
-    echo "${actions[index]}"
+reverse=0
+[[ ${#number} -eq 5 ]] && reverse=1 && number=$(rev<<< "${number:1:4}") || number=$(rev<<< "$number")
 
-
+declare -a output=()
+for ((i=0; i<${#number}; i++)); do
+    (( ${number:$i:1} )) && output+=("${actions[i]}")
 done
 
+if (( reverse )); then
+    reversed_output=()
+    for ((i=${#output[@]}-1; i>=0; i--)); do
+        reversed_output+=("${output[i]}")
+    done
+    echo "${reversed_output[*]}"
+else
+    echo "${output[*]}"
+fi
+
+# Good solution:
+# events=(wink 'double blink' 'close your eyes' jump)
+
+# ((reverse = $1 >> 4))
+
+# for i in "${!events[@]}"; do
+#     if (($1 >> i & 1)); then
+#         # echo $i "${events[i]}"
+#         ((reverse)) && result="${events[i]},$result" || result+="${events[i]},"
+#     fi
+# done
+
+# echo "${result%,}"
