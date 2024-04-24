88d4bca34d035f02c94ee528c0fe0c1b1f8b2f7c
Matt McCullough <mattrmcc@gmail.com>
2024-04-24 15:33:13 -0500

    submit dnd_character.sh

--- bash/dnd-character/dnd_character.sh
+++ bash/dnd-character/dnd_character.sh
@@ -1,24 +1,45 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+rollDie () {
+    echo $(( RANDOM % 6 + 1 ))
+}
+
+rollDice () {
+    rolls+=("$(rollDie)")
+    for i in {0..2}; do
+        roll=$(rollDie) && (( roll >= "${rolls[0]}" )) && rolls+=("$roll") \
+        || rolls=("$roll" "${rolls[@]}")
+    done
+    for i in "${rolls[@]:1:3}"; do (( sum+=i )); done
+    echo "$sum"
+}
+
+modify () {
+    # mod=$(($1 - 10)) && [[ $mod -lt 0 ]] && mod=$(bc -l <<<"$mod--")
+    # bc <<<"scale=0;$mod/2"
+
+    local -i n=$1
+    # a little trickery to ensure we round *down*
+    ((n < 10)) && ((n--))
+    echo $(((n - 10) / 2))
+}
+
+main () {
+    case "$1" in
+        'modifier')
+            modify "$2"
+        ;;
+        'generate')
+            abilities=(strength dexterity constitution intelligence wisdom \
+                        charisma)
+            declare -A character
+            for ability in "${abilities[@]}"; do 
+                character[$ability]=$(rollDice)
+                echo "$ability" "${character[$ability]}"
+            done
+            echo hitpoints $((10 + $(modify "${character[constitution]}")))
+        ;;
+    esac
+}
+
+main "$@"
