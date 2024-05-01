e719abdf56d050cd6dcadff7c84d7767e46ff255
Matt McCullough <mattrmcc@gmail.com>
2024-05-01 13:43:33 -0500

    submit tournament.sh

--- bash/tournament/tournament.sh
+++ bash/tournament/tournament.sh
@@ -1,28 +1,13 @@
 #!/usr/bin/env bash
 
-# Result format:
-# Team                           | MP |  W |  D |  L |  P
-# Devastating Donkeys            |  3 |  2 |  1 |  0 |  7
-# Allegoric Alaskans             |  3 |  2 |  0 |  1 |  6
-# Blithering Badgers             |  3 |  1 |  0 |  2 |  3
-# Courageous Californians        |  3 |  0 |  1 |  2 |  1
-
-# processRecord() {
-
-# }
-
 declare -A table
 while read -r line; do
-    # echo "$line"
+    if [ -z "$line" ]; then break; fi
     IFS=';'; read -ra record <<< "$line"
-    printf -v team1 '%-30s' "${record[0]}"
-    printf -v team2 '%-30s' "${record[1]}"
-    if [[ ! ${!table[*]} =~ $team1 ]]; then
-        table["$team1"]="0,0,0,0,0"
-    fi
-    if [[ ! ${!table[*]} =~ $team2 ]]; then
-        table["$team2"]="0,0,0,0,0"
-    fi
+    team1="${record[0]}"
+    team2="${record[1]}"
+    [[ ! ${!table[*]} =~ $team1 ]] && table["$team1"]="0,0,0,0,0"
+    [[ ! ${!table[*]} =~ $team2 ]] && table["$team2"]="0,0,0,0,0"
     IFS=','; 
     read -r -a team1Record <<< "${table[$team1]}"
     read -r -a team2Record <<< "${table[$team2]}"
@@ -48,24 +33,14 @@ while read -r line; do
     esac
     table["$team1"]="${team1Record[*]}"
     table["$team2"]="${team2Record[*]}"
-done # < "$1"
 
-header="Team                           | MP |  W |  D |  L |  P"
-echo "$header"
+done < "${1:-/dev/stdin}"
 
-# Exit if no input data
+echo "Team                           | MP |  W |  D |  L |  P"
 if [ "${#table[@]}" -lt 2 ]; then exit 0; fi
-
-# IFS=$'\n' table=($(sort <<<"${table[*]}"))
-# unset IFS
 
 for team in "${!table[@]}"; do
     read -r -a teamRecord <<< "${table[$team]}"
-    # IFS="| "
-    # echo "$team${teamRecord[*]}" | column -t -N 'Team,MP,W,D,L,P' -o '|' 
-    echo "$team" "| " "${teamRecord[0]}" "| " \
-    "${teamRecord[1]}" "| " "${teamRecord[2]}" "| " "${teamRecord[3]}" \
-    "| " "${teamRecord[4]}"
-done
-
-
+    printf '%-31s|%3i |%3i |%3i |%3i |%3i\n' "$team" "${teamRecord[0]}" \
+    "${teamRecord[1]}" "${teamRecord[2]}" "${teamRecord[3]}" "${teamRecord[4]}"
+done | sort -t "|" -k6nr -k1
