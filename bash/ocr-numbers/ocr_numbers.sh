a020330590e4082d813fd61e9c8b6c0a59eaa215
Matt McCullough <mattrmcc@gmail.com>
2024-05-05 12:40:02 -0500

    add ocr_numbers exercise

---
+++ bash/ocr-numbers/ocr_numbers.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+ zero=(" _ " "| |" "|_|" "   ")
+  one=("   " "  |" "  |" "   ")
+  two=(" _ " " _|" "|_ " "   ")
+three=(" _ " " _|" " _|" "   ")
+ four=("   " "|_|" "  |" "   ")
+ five=(" _ " "|_ " " _|" "   ")
+  six=(" _ " "|_ " "|_|" "   ")
+seven=(" _ " "  |" "  |" "   ")
+eight=(" _ " "|_|" "|_|" "   ")
+ nine=(" _ " "|_|" "  |" "   ")
+
+
+
+
+# printf " _ \n| |\n|_|\n\n"  #zero
+# printf "   \n  |\n  |\n\n"  #one
+# printf " _ \n _|\n|_ \n\n"  #two
+# printf " _ \n _|\n _|\n\n"  #three
+# printf "   \n|_|\n  |\n\n"  #four
+# printf " _ \n|_ \n _|\n\n"  #five
+# printf " _ \n|_ \n|_|\n\n"  #six
+# printf " _ \n  |\n  |\n\n"  #seven
+# printf " _ \n|_|\n|_|\n\n"  #eight
+# printf " _ \n|_|\n  |\n\n"  #nine
+
+# The following comments should help you get started:
+# - Bash is flexible. You may use functions or write a "raw" script.
+#
+# - Complex code can be made easier to read by breaking it up
+#   into functions, however this is sometimes overkill in bash.
+#
+# - You can find links about good style and other resources
+#   for Bash in './README.md'. It came with this exercise.
+#
+#   Example:
+#   # other functions here
+#   # ...
+#   # ...
+#
+#   main () {
+#     # your main function code here
+#   }
+#
+#   # call main with all of the positional arguments
+#   main "$@"
+#
+# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
