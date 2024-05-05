735f9ff9bd8916849c9b31763d69874b401a7fba
Matt McCullough <mattrmcc@gmail.com>
2024-05-04 20:42:19 -0500

    submit clock exercise

--- bash/clock/clock.sh
+++ bash/clock/clock.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 die () {
-    echo "$1"
-    exit 1
+    echo "$1"; exit 1
 }
 
 [[ $# != 2 && $# != 4 ]] && die 'invalid arguments'
 hour="$1"
 minute="$2"
+re='^-?[0-9]+$'
+! [[ $1 =~ $re ]] && die 'non-numeric'
+! [[ $2 =~ $re ]] && die 'non-numeric'
 if [[ $# = 4 ]]; then
     operator="$3"
     xminutes="$4"
+    ! [[ $4 =~ $re ]] && die 'non-numeric'
     if [[ $operator == "+" ]]; then
         (( minute+=xminutes ))
     elif [[ $operator == "-" ]]; then
@@ -18,14 +21,11 @@ if [[ $# = 4 ]]; then
     else die 'invalid arguments'
     fi
 fi
-# echo "$hour $minute"
 
 modMin=$(( minute % 60 ))
 updatedHour=$((((minute / 60) % 12 + (hour % 24)) % 24))
 
-# echo "$modHour $divMin $modMin $updatedHour"
-
 (( modMin < 0)) && modMin=$((60+modMin)) && ((updatedHour--))
-(( updatedHour < 0 )) && updatedHour=$((24+"$updatedHour"))
+(( updatedHour < 0 )) && updatedHour=$((24+updatedHour))
 
 printf '%02d:%02d\n' $updatedHour $modMin
