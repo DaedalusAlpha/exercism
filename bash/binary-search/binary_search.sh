93b9992361780d04598c4e3e31ffc55f082b5751
Matt McCullough <mattrmcc@gmail.com>
2024-05-10 10:23:39 -0500

    submit binary_search exercise

--- bash/binary-search/binary_search.sh
+++ bash/binary-search/binary_search.sh
@@ -1,44 +1,22 @@
 #!/usr/bin/env bash
 
+declare -a list
 binarySearch() {
-
-    local searchItem="$1"
-    local left="$2"
-    local right="$3"
-    shift 3
-    local list=( "$@" )
-    # echo "$searchItem"
-    # echo "$left"
-    # echo "$right"
-    # echo "${list[@]}"
-    # exit 0
-    # local length="${#list}"
-    # [[ $length -eq 0 ]] && echo "-1" && exit 0
-    local searchIndex=$(( ( right - left ) / 2 ))
-
-    echo "$searchIndex"
-    # exit 0
-
+    local searchItem="$1" left="$2" right="$3"
+    [[ left -ge right ]] && echo "-1" && exit 0
+    local searchIndex=$(( ( left + right ) / 2 ))
     if [[ ${list[searchIndex]} -eq searchItem ]]; then #item found
         echo "$searchIndex"; exit 0
     elif (( list[searchIndex] > searchItem )); then #item is less than middle
-        echo "< $searchItem" "$left" "$searchIndex" "${list[@]}"
-        # 0-3
-        binarySearch "$searchItem" "$left" "$searchIndex" "${list[@]}"
+        binarySearch "$searchItem" "$left" $searchIndex
     else #item is more than middle
-        echo "> $searchItem" "$(( searchIndex++ ))" "$right" "${list[@]}"
-        # 4-7
-        binarySearch "$searchItem" "$(( searchIndex++ ))" "$right" "${list[@]}"
+        binarySearch "$searchItem" $(( searchIndex+=1 )) "$right"
     fi
-
-
 }
 
 main () {
-    local searchItem="$1"; shift; 
-    local list=( "$@" )
-    echo "$searchItem" 0 "${#list[@]}" "${list[@]}"
-    binarySearch "$searchItem" 0 "${#list[@]}" "${list[@]}"
+    local searchItem="$1"; shift; list=( "$@" )
+    binarySearch "$searchItem" 0 "${#list[@]}"
 }
 
 main "$@"
