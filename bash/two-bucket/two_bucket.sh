55aef3265e7402ed64adbbe66ca065ed4241077a
Matt McCullough <mattrmcc@gmail.com>
2024-05-31 12:39:30 -0500

    submit two_bucket.sh

--- bash/two-bucket/two_bucket.sh
+++ bash/two-bucket/two_bucket.sh
@@ -1,89 +1,51 @@
 #!/usr/bin/env bash
 
-die () {
-    echo "invalid goal"; exit 1
+startWithOne () { 
+    while (( oneCurrent != target && twoCurrent != target )); do
+        (( twoSpace = twoCapacity - twoCurrent ))
+        (( moves++ ))
+        if [[ $oneCurrent -gt 0 && $twoCurrent -lt $twoCapacity && 
+            $oneCurrent -ne $twoSpace ]]; then
+            while (( twoCurrent < twoCapacity && oneCurrent > 0 ))
+            do ((oneCurrent-- && twoCurrent++ )); done
+        elif [[ $oneCurrent -eq 0 && $twoCurrent -lt $twoCapacity ]]
+            then oneCurrent=${oneCapacity}
+        elif [[ $twoCurrent -eq $twoCapacity && 0 -lt $oneCurrent && 
+            $oneCurrent -lt $oneCapacity ]]; then twoCurrent=0
+        else echo "invalid goal"; exit 1
+        fi
+    done
 }
 
-
-fillOne() {
-    oneCurrent=${oneCapacity}
-    ((moves++))
-    display "$FUNCNAME"
+startWithTwo () { 
+    while (( oneCurrent != target && twoCurrent != target )); do
+        (( moves++ ))
+        if [[ $oneCurrent -eq $oneCapacity ]]; then oneCurrent=0
+        elif [[ $twoCurrent -eq 0 ]]; then twoCurrent=${twoCapacity}
+        else
+            while (( oneCurrent < oneCapacity && twoCurrent > 0 ))
+            do ((twoCurrent-- && oneCurrent++ )); done
+        fi
+    done
 }
 
-fillTwo() {
-    twoCurrent=${twoCapacity}
-    ((moves++))
-    display "$FUNCNAME"
-}
+target="$3" moves=1
+(( target > $1 && target > $2 )) && echo "invalid goal" && exit 1
+oneCapacity="$1" oneCurrent=0
+twoCapacity="$2" twoCurrent=0
+case "$4" in 
+    one)
+        oneCurrent=${oneCapacity}
+        [[ $target -eq $twoCapacity ]] && ((moves++)) && 
+        twoCurrent=${twoCapacity}
+        startWithOne ;;
+    two) twoCurrent=${twoCapacity}
+        [[ $target -eq $oneCapacity ]] && ((moves++)) && 
+        oneCurrent=${oneCapacity}
+        startWithTwo
+esac
 
-emptyOne() {
-    oneCurrent=0
-    ((moves++))
-    display "$FUNCNAME"
-}
-
-emptyTwo() {
-    twoCurrent=0
-    ((moves++))
-    display "$FUNCNAME"
-}
-fillOneFromTwo() { 
-    while (( oneCurrent < oneCapacity && twoCurrent > 0 ))
-    do ((twoCurrent-- && oneCurrent++ )); done
-    ((moves++))
-    display "$FUNCNAME"
-}
-
-fillTwoFromOne() { 
-    while (( twoCurrent < twoCapacity && oneCurrent > 0 ))
-    do ((oneCurrent-- && twoCurrent++ )); done
-    ((moves++))
-    display "$FUNCNAME"
-}
-
-display () { 
-echo "$1"
-echo "One - Current: ${oneCurrent}, Capacity: ${oneCapacity}"
-echo "Two - Current: ${twoCurrent}, Capacity: ${twoCapacity}"
-}
-
-# if [ goalBucket=="one" ]
-
-target="$3"
-# Goal larger than both buckets is impossible
-(( target > $1 && target > $2 )) && die
-oneCapacity="$1"
-oneCurrent=0
-twoCapacity="$2"
-twoCurrent=0
-moves=0
-goalBucket=""
-otherBucket=0
-case "$4" in one) fillOne ;; two) fillTwo; esac
-
-while (( oneCurrent != target && twoCurrent != target )); do
-    if [[ $4 == "one" ]]; then
-        (( oneCurrent = oneCapacity && twoCurrent < twoCapacity && oneCurrent != (twoCapacity - twoCurrent) )) && fillTwoFromOne && continue
-        (( oneCurrent = 0 && twoCurrent < twoCapacity )) && fillOne && continue
-        (( twoCurrent = twoCapacity && 0 < oneCurrent < oneCapacity )) && emptyTwo && continue
-    fi
-    # [[ $twoCapacity -eq $twoCurrent && $oneCurrent -lt $oneCapacity ]] && fillOneFromTwo
-
-    
-    # echo 1
-done
-
-(( oneCurrent = target )) && goalBucket=one && otherBucket=$twoCurrent
-(( twoCurrent = target )) && goalBucket=two && otherBucket=$oneCurrent
-
-printf 'moves: %d, goalBucket: %s, otherBucket: %d\n' "$moves" "$goalBucket" "$otherBucket"
-
-# fillTwoFromOne
-# fillOne
-# fillTwoFromOne
-# emptyTwo
-# fillTwoFromOne
-# fillOne
-# fillTwoFromOne
-# echo "$moves"
+[[ $oneCurrent -eq $target ]] && goalBucket=one && otherBucket=$twoCurrent
+[[ $twoCurrent -eq $target ]] && goalBucket=two && otherBucket=$oneCurrent
+printf 'moves: %d, goalBucket: %s, otherBucket: %d\n' \
+    "$moves" "$goalBucket" "$otherBucket"
