ae281e2bb1e9a9b994c69f966c2169eac21e780a
Matt McCullough <mattrmcc@gmail.com>
2024-05-31 11:24:26 -0500

    update two_bucket.sh

--- bash/two-bucket/two_bucket.sh
+++ bash/two-bucket/two_bucket.sh
@@ -1,15 +1,89 @@
 #!/usr/bin/env bash
 
-sizeOne="$1" sizeTwo="$2"
-target="$3" first="$4"
-
-output="moves: 4, goalBucket: one, otherBucket: 5"
-
-bucketOne[capacity]="$1"
-bucketOne[current]=0
-bucketTwo[capacity]="$2"
-bucketTwo[current]=0
-
-fillSmall() {
-    
+die () {
+    echo "invalid goal"; exit 1
 }
+
+
+fillOne() {
+    oneCurrent=${oneCapacity}
+    ((moves++))
+    display "$FUNCNAME"
+}
+
+fillTwo() {
+    twoCurrent=${twoCapacity}
+    ((moves++))
+    display "$FUNCNAME"
+}
+
+emptyOne() {
+    oneCurrent=0
+    ((moves++))
+    display "$FUNCNAME"
+}
+
+emptyTwo() {
+    twoCurrent=0
+    ((moves++))
+    display "$FUNCNAME"
+}
+fillOneFromTwo() { 
+    while (( oneCurrent < oneCapacity && twoCurrent > 0 ))
+    do ((twoCurrent-- && oneCurrent++ )); done
+    ((moves++))
+    display "$FUNCNAME"
+}
+
+fillTwoFromOne() { 
+    while (( twoCurrent < twoCapacity && oneCurrent > 0 ))
+    do ((oneCurrent-- && twoCurrent++ )); done
+    ((moves++))
+    display "$FUNCNAME"
+}
+
+display () { 
+echo "$1"
+echo "One - Current: ${oneCurrent}, Capacity: ${oneCapacity}"
+echo "Two - Current: ${twoCurrent}, Capacity: ${twoCapacity}"
+}
+
+# if [ goalBucket=="one" ]
+
+target="$3"
+# Goal larger than both buckets is impossible
+(( target > $1 && target > $2 )) && die
+oneCapacity="$1"
+oneCurrent=0
+twoCapacity="$2"
+twoCurrent=0
+moves=0
+goalBucket=""
+otherBucket=0
+case "$4" in one) fillOne ;; two) fillTwo; esac
+
+while (( oneCurrent != target && twoCurrent != target )); do
+    if [[ $4 == "one" ]]; then
+        (( oneCurrent = oneCapacity && twoCurrent < twoCapacity && oneCurrent != (twoCapacity - twoCurrent) )) && fillTwoFromOne && continue
+        (( oneCurrent = 0 && twoCurrent < twoCapacity )) && fillOne && continue
+        (( twoCurrent = twoCapacity && 0 < oneCurrent < oneCapacity )) && emptyTwo && continue
+    fi
+    # [[ $twoCapacity -eq $twoCurrent && $oneCurrent -lt $oneCapacity ]] && fillOneFromTwo
+
+    
+    # echo 1
+done
+
+(( oneCurrent = target )) && goalBucket=one && otherBucket=$twoCurrent
+(( twoCurrent = target )) && goalBucket=two && otherBucket=$oneCurrent
+
+printf 'moves: %d, goalBucket: %s, otherBucket: %d\n' "$moves" "$goalBucket" "$otherBucket"
+
+# fillTwoFromOne
+# fillOne
+# fillTwoFromOne
+# emptyTwo
+# fillTwoFromOne
+# fillOne
+# fillTwoFromOne
+# echo "$moves"
