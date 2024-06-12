62de868f4a6a1a50faac0a30792acbfdcfcd43b3
Matt McCullough <mattrmcc@gmail.com>
2024-06-12 10:38:48 -0500

    updated diffie_hellman

--- bash/diffie-hellman/diffie_hellman.sh
+++ bash/diffie-hellman/diffie_hellman.sh
@@ -1,47 +1,26 @@
 #!/usr/bin/env bash
 
 private_key() {
-    echo 1
+    echo $(( RANDOM % ($1 - 2) + 2 ))
 }
 
-public_key() {
-    local p="$1" g="$2" a="$3"
-    echo $(( (g ** a) % p ))
+exchange() {
+    local p="$1" g="$2" a="$3" i res=1
+	for ((i=0; i<a;i++)); do
+		res=$(( (res * g) % p))
+	done
+    echo "$res"
 }
 
 main() {
     action=$1; shift
     case "$action" in
-        publicKey)
-            public_key "$@";;
+        publicKey|secret)
+            exchange "$@";;
         privateKey) 
             private_key "$@";;
-        secret) ;;
         *)
     esac
 }
 
 main "$@"
-
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
