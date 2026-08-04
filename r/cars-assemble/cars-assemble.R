ccfbea3edeb7092a9b8d6c03269a18000c6c51d3
Matt McCullough <mattrmcc@gmail.com>
2026-08-04 11:17:40 -0500

    submit R cars-assemble

---
+++ r/cars-assemble/cars-assemble.R
@@ -0,0 +1,21 @@
+success_rate <- function(speed) {
+    if (speed == 0) {
+        0
+    } else if (speed >= 1 && speed <= 4) {
+        1
+    } else if (speed >= 5 && speed <= 8) {
+        .9
+    } else if (speed == 9) {
+        .8
+    } else if (speed == 10) {
+        .77
+    }
+}
+
+production_rate_per_hour <- function(speed) {
+    success_rate(speed) * speed * 221
+}
+
+working_items_per_minute <- function(speed) {
+    (production_rate_per_hour(speed) %/% 60)
+}
