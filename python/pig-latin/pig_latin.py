8274430d4ec6663f7780132d7fbd0e1c4138665b
Matt McCullough <matt@Matts-MacBook-Air.local>
2026-08-10 20:59:55 -0500

    Submit Python Pig Latin

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,31 +1,31 @@
+"""Functions to translate phrases/words into pig latin"""
+
 def translate(text):
-    vowels = "aeiou"
-    
- 
-    
-    
-    v_index = -1
-    output = text
-    for i, letter in enumerate(text):
+    """Translate a phrase into pig latin"""
+    phrase = text.split(" ")
+    for index, word in enumerate(phrase):
+        phrase[index] = translate_word(word)
+    return " ".join(phrase)
+
+def translate_word(text):
+    """Translate a word into pig latin"""
+    vowels = "aeiouy"
+    vowel, vowel_index = "", -1
+    for index, letter in enumerate(text):
         if letter in vowels:
-            v_index = i
+            vowel, vowel_index = letter, index
             break
-    y_index = -1
-    for i, letter in enumerate(text):
-        if letter == "y":
-            y_index = i
-            break
-    # Rule 1, vowel at start or special cases
-    if v_index == 0 or text[0:2] in ("xr","yt","ay"):
+    output = ""
+    if text[0:2] in {"xr","yt"}:
         output = text + "ay"
-    elif v_index >= 1 or y_index > v_index: #There is a vowel, and y is not first
-        # Rule 3, QU after 0+ consonants
-        if text[v_index-1:v_index+1] == "qu":
-            output = text[v_index+1:] + text[0:v_index+1] + "ay"
-        # Rule 2, vowel after 1+ consonants
-        elif "y" not in text[0:v_index]:
-            output = text[v_index:] + text[0:v_index] + "ay"   
-        # Rule 4, Y after 1+ consonants    
-        elif "y" in text:
-            output = text[y_index:] + text[0:y_index] + "ay" 
-    return output        
+    elif vowel_index == 0:
+        if vowel == "y":
+            output = text[vowel_index+1:] + text[0:vowel_index+1] + "ay" 
+        else:
+            output = text + "ay"
+    elif vowel_index >= 1:
+        if text[vowel_index-1:vowel_index+1] == "qu":
+            output = text[vowel_index+1:] + text[0:vowel_index+1] + "ay"
+        else:
+            output = text[vowel_index:] + text[0:vowel_index] + "ay" 
+    return output
